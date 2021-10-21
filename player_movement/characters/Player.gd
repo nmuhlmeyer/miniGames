822e04c3a3d14cb169eb8396be6accac6e9d1045
nmuhlmeyer <nmuhlmeyer@gmail.com>
2021-10-20 21:18:56 -0700

    player movement commit

--- player_movement/characters/Player.gd
+++ player_movement/characters/Player.gd
@@ -1,13 +1,22 @@
 extends KinematicBody2D
 
 var speed = 5000
-var gravity = 2000
-var velocity= Vector2()
+#var gravity = 2000
+#var velocity= Vector2()
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
-	velocity = Vector2()
+	#velocity = Vector2()
 	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	if direction.x > 0:
+		$Sprite.set_flip_h(false)
+	elif direction.x < 0:
+		$Sprite.set_flip_h(true)
+	if direction.y < 0:
+		$Sprite.set_flip_v(false)
+	elif direction.y > 0:
+		$Sprite.set_flip_v(true)
 	direction = direction.normalized()
 	print(direction)
 	
